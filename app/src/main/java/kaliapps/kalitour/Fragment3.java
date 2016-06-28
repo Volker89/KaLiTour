408969a9bc10e0ed97361a852571d0eb346123b9
Volker89 <G0k!l3!n>
2016-06-28 22:54:59 +0200

    Fragment3 bearbeitet

--- app/src/main/java/kaliapps/kalitour/Fragment3.java
+++ app/src/main/java/kaliapps/kalitour/Fragment3.java
@@ -27,34 +27,12 @@ public class Fragment3 extends Fragment {
         View rootView = inflater.inflate(R.layout.frag3, container,false);
 
         final MediaPlayer sound = MediaPlayer.create(getActivity(), R.raw.hund);
+        final MediaPlayer station1 = MediaPlayer.create(getActivity(), R.raw.station1);
 
         Button play, pause, stop;
 
         play = (Button) rootView.findViewById(R.id.play);
         stop = (Button) rootView.findViewById(R.id.stop);
-
-        play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sound.start();
-            }
-        });
-
-        stop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sound.pause();
-            }
-        });
-
-        Button button = (Button) rootView.findViewById(R.id.button7);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getActivity(), MapActivity.class);
-                startActivity(i);
-            }
-        });
 
         Context thiscontext;
         List<Station> stationen;
@@ -75,40 +53,214 @@ public class Fragment3 extends Fragment {
         switch (text) {
             case "Altes Rathaus":
                 momentaneStation = stationen.get(0);
+
+                play.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        station1.start();
+                    }
+                });
+
+                stop.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        station1.pause();
+                    }
+                });
+
                 break;
             case "Johannstrasse":
                 momentaneStation = stationen.get(1);
+
+                play.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.start();
+                    }
+                });
+
+                stop.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.pause();
+                    }
+                });
+
                 break;
             case "Alfredstrasse":
                 momentaneStation = stationen.get(2);
+
+                play.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.start();
+                    }
+                });
+
+                stop.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.pause();
+                    }
+                });
+
                 break;
             case "Georgstrasse":
                 momentaneStation = stationen.get(3);
+
+                play.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.start();
+                    }
+                });
+
+                stop.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.pause();
+                    }
+                });
+
                 break;
             case "Markt":
                 momentaneStation = stationen.get(4);
+
+                play.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.start();
+                    }
+                });
+
+                stop.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.pause();
+                    }
+                });
+
                 break;
             case "Antonstrasse":
                 momentaneStation = stationen.get(5);
+
+                play.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.start();
+                    }
+                });
+
+                stop.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.pause();
+                    }
+                });
+
                 break;
             case "Lotharstrasse":
                 momentaneStation = stationen.get(6);
+
+                play.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.start();
+                    }
+                });
+
+                stop.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.pause();
+                    }
+                });
+
                 break;
             case "Barbarastrasse":
                 momentaneStation = stationen.get(7);
+
+                play.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.start();
+                    }
+                });
+
+                stop.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.pause();
+                    }
+                });
+
                 break;
             case "Vinnstrasse":
                 momentaneStation = stationen.get(8);
+
+                play.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.start();
+                    }
+                });
+
+                stop.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.pause();
+                    }
+                });
+
                 break;
             case "Maxstrasse":
                 momentaneStation = stationen.get(9);
+
+                play.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.start();
+                    }
+                });
+
+                stop.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.pause();
+                    }
+                });
+
                 break;
             default:
                 momentaneStation = stationen.get(10);
+
+                play.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.start();
+                    }
+                });
+
+                stop.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        sound.pause();
+                    }
+                });
+
         }
 
         TextView stationName = (TextView) rootView.findViewById(R.id.textView_stationNameAudio);
         stationName.setText(momentaneStation.getName());
+
+        Button button = (Button) rootView.findViewById(R.id.button7);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), MapActivity.class);
+                startActivity(i);
+            }
+        });
 
         return rootView;
     }
