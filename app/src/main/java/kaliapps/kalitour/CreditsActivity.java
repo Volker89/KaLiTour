91c06c7e6f9ad5d493d4887d90270a9bcf053b41
Volker89 <G0k!l3!n>
2016-06-28 16:01:04 +0200

    Credits finalisiert

--- app/src/main/java/kaliapps/kalitour/CreditsActivity.java
+++ app/src/main/java/kaliapps/kalitour/CreditsActivity.java
@@ -1,7 +1,10 @@
 package kaliapps.kalitour;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreditsActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class CreditsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+
+        ((Button)findViewById(R.id.button3)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(CreditsActivity.this, MapActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
