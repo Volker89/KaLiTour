bf515297827679400b65803c9a0bb45b61fc8ae8
Volker89 <G0k!l3!n>
2016-06-28 15:30:49 +0200

    Anleitung finalisiert

--- app/src/main/java/kaliapps/kalitour/InstructActivity.java
+++ app/src/main/java/kaliapps/kalitour/InstructActivity.java
@@ -1,7 +1,10 @@
 package kaliapps.kalitour;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InstructActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class InstructActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instruct);
+
+        ((Button)findViewById(R.id.button2)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(InstructActivity.this, MapActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
