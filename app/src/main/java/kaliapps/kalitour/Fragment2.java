b7f47a564415a10798d41a25a7c7310decc05a2b
Volker89 <G0k!l3!n>
2016-06-28 16:53:41 +0200

    Bilder Aufruf bearbeitet Fragment2

--- app/src/main/java/kaliapps/kalitour/Fragment2.java
+++ app/src/main/java/kaliapps/kalitour/Fragment2.java
@@ -44,40 +44,64 @@ public class Fragment2 extends Fragment {
                 momentaneStation = stationen.get(0);
 
                 ImageView image0 = (ImageView) rootView.findViewById(R.id.imageView_stationBild);
-                image0.setImageResource(R.drawable.weisseriesen);
-
-                ImageView image2 = (ImageView) rootView.findViewById(R.id.imageView_stationBild2);
-                image2.setImageResource(R.drawable.weisseriesen);
+                image0.setImageResource(R.drawable.bild_station1);
                 break;
             case "Johannstrasse":
                 momentaneStation = stationen.get(1);
 
                 ImageView image1 = (ImageView) rootView.findViewById(R.id.imageView_stationBild);
-                image1.setImageResource(R.drawable.christianstr);
+                image1.setImageResource(R.drawable.bild_station2);
                 break;
             case "Alfredstrasse":
                 momentaneStation = stationen.get(2);
+
+                ImageView image2 = (ImageView) rootView.findViewById(R.id.imageView_stationBild);
+                image2.setImageResource(R.drawable.bild_station3);
                 break;
             case "Georgstrasse":
                 momentaneStation = stationen.get(3);
+
+                ImageView image3 = (ImageView) rootView.findViewById(R.id.imageView_stationBild);
+                image3.setImageResource(R.drawable.bild_station4);
                 break;
             case "Markt":
                 momentaneStation = stationen.get(4);
+
+                ImageView image51 = (ImageView) rootView.findViewById(R.id.imageView_stationBild);
+                image51.setImageResource(R.drawable.bild_station5);
+
+                ImageView image52 = (ImageView) rootView.findViewById(R.id.imageView_stationBild2);
+                image52.setImageResource(R.drawable.bild_station5_2);
                 break;
             case "Antonstrasse":
                 momentaneStation = stationen.get(5);
+
+                ImageView image6 = (ImageView) rootView.findViewById(R.id.imageView_stationBild);
+                image6.setImageResource(R.drawable.bild_station6);
                 break;
             case "Lotharstrasse":
                 momentaneStation = stationen.get(6);
                 break;
             case "Barbarastrasse":
                 momentaneStation = stationen.get(7);
+
+                ImageView image7 = (ImageView) rootView.findViewById(R.id.imageView_stationBild);
+                image7.setImageResource(R.drawable.bild_station8);
                 break;
             case "Vinnstrasse":
                 momentaneStation = stationen.get(8);
+
+                ImageView image81 = (ImageView) rootView.findViewById(R.id.imageView_stationBild);
+                image81.setImageResource(R.drawable.bild_station9);
+
+                ImageView image82 = (ImageView) rootView.findViewById(R.id.imageView_stationBild2);
+                image82.setImageResource(R.drawable.bild_station9_2);
                 break;
             case "Maxstrasse":
                 momentaneStation = stationen.get(9);
+
+                ImageView image9 = (ImageView) rootView.findViewById(R.id.imageView_stationBild);
+                image9.setImageResource(R.drawable.bild_station10);
                 break;
             default:
                 momentaneStation = stationen.get(10);
